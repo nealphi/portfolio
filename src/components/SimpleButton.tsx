c41832991431e95daecfbc413f79eed1fddff8a6
Negin Alphi <negin.alipanahi@gmail.com>
2024-05-27 15:27:35 -0400

    simppleButton revised

--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -1,11 +1,33 @@
-import { Button } from "@chakra-ui/react";
-
-interface SimpleButtonProps {
-  children: React.ReactNode;
+import { Box } from "@chakra-ui/react";
+interface Props {
+  children: string;
   onClick: () => void;
 }
 
-const SimpleButton = ({ children, onClick }: SimpleButtonProps) => {
+const SimpleButton = ({ children, onClick }: Props) => {
+  return (
+    <Box
+      as="button"
+      width="130px"
+      height="30px"
+      transition="all 0.7s cubic-bezier(.08,.52,.52,1)"
+      borderRadius="5px"
+      fontSize="14px"
+      fontWeight="bold"
+      bg="#212C41"
+      color="#B6B6B6"
+      justifyContent="center"
+      _hover={{ color: "white", bg: "#006a67", w: "170px" }}
+      _active={{
+        bg: "gray",
+        transform: "scale(0.98)",
+        borderColor: "#bec3c9",
+      }}
+      onClick={() => onClick()}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default SimpleButton;
