aca5f3359fc27e44f4e322f3786f421d3d36bc88
Negin Alphi <negin.alipanahi@gmail.com>
2024-05-27 20:19:34 -0400

    pdf pathway updated

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -2,7 +2,7 @@
 const PDFViewer = () => {
  return (
  <div>
- <iframe className="lazy player-ratio" src='src/assets/Frontend developer - Negin Alipanahi.pdf' width={"100%"} height={"2000"}/>
+ <iframe className="lazy player-ratio" src='public/Frontend developer - Negin Alipanahi.pdf' width={"100%"} height={"2000"}/>
  </div>
  );
 };
