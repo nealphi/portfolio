7b70aaff899ecf9089340ba1ea9433c3d05d1457
Negin Alphi <negin.alipanahi@gmail.com>
2024-05-27 20:28:14 -0400

    changes made

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -2,7 +2,7 @@
 const PDFViewer = () => {
  return (
  <div>
- <iframe className="lazy player-ratio" src='/Users/negin/portfolio/public/NeginAlipanahi.pdf' width={"100%"} height={"2000"}/>
+ <iframe src='/Users/negin/portfolio/public/NeginAlipanahi.pdf' width={"100%"} height={"2000"}/>
  </div>
  );
 };
